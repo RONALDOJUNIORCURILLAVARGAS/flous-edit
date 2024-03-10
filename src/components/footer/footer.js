129c2036525043ee551b2e7673b104aff5290cf7
ronaldo <ronaldocurillavargas@gmail.com>
2024-03-09 22:53:47 -0500

    cambios

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,43 +1,59 @@
-
-import logoBlanco from '../../assets/logo-blaco.png'
-import './footer.css'
-import navIcon1 from '../../assets/svg/facebook.svg'
-import navIcon2 from '../../assets/svg/linkedin.svg'
+import logoBlanco from "../../assets/logo-blaco.png";
+import "./footer.css";
+import navIcon1 from "../../assets/svg/facebook.svg";
+import navIcon2 from "../../assets/svg/linkedin.svg";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <div className='footer-detalle'>
-
-                <div className='detalle-foo'>
-                    <br />
-                    <br />
-                    <img src={logoBlanco} alt="logo" className="logo-img" />
-                    <p>Our mission is to redefine business possibilities by leveraging technology as a catalyst for innovation and growth.</p>
-                </div>
-
-                <div className='detalle-foo'>
-                    <h2>Servicios</h2>
-                    <a href="https://www.ejemplo.com">Inicio</a> <br />
-                    <a href="#servicios1">Servicios</a> <br />
-                    <a href="https://bit.ly/4arqpzp">Nosotros</a>  <br />
-                    <a href="https://bit.ly/4arqpzp">Contáctenos</a>
-                </div>
-
-                <div className='detalle-foo'>
-                    <h2>Contáctanos</h2>
-                    <span> admin@flowupsolutions.com</span> <br />
-                    <span>+51 991316320 </span><br />
-                    <a href="https://www.facebook.com/profile.php?id=61554718285482&locale=es_LA" target="_blank"><img src={navIcon1} alt="" style={{padding: '10px 20px'}}/></a>
-                    <a href="https://www.linkedin.com/company/flow-up-solutions/?viewAsMember=true" target="_blank"><img src={navIcon2} alt="" style={{padding: '10px 20px'}}/></a>
-                </div>
-
-            </div>
-            <div> @2023 Flous - Todos los derechos reservados
-            </div>
+  return (
+    <div className="footer px-[20px] md:px-0 pb-[20px]">
+      <div className="footer-detalle max-w-6xl mx-auto py-[50px] md:grid md:grid-cols-3 gap-4">
+        <div className="detalle-foo text-start">
+          <img src={logoBlanco} alt="logo" className="logo-img w-[150px]" />
+          <p className="mt-[20px]">
+            Our mission is to redefine business possibilities by leveraging
+            technology as a catalyst for innovation and growth.
+          </p>
         </div>
-    )
-}
 
+        <div className="detalle-foo text-start mt-[20px] md:mt-0">
+          <h2 className="font-bold text-[20px]">Servicios</h2>
+          <div className="flex flex-col gap-[10px]">
+            <a href="https://www.ejemplo.com">Inicio</a>
+            <a href="#servicios1">Servicios</a>
+            <a href="https://bit.ly/4arqpzp">Nosotros</a>
+            <a href="https://bit.ly/4arqpzp">Contáctenos</a>
+          </div>
+        </div>
+
+        <div className="detalle-foo text-start mt-[20px] md:mt-0 flex flex-col gap-[6px]">
+          <h2 className="font-bold text-[20px]">Contáctanos</h2>
+          <div>
+            <span> admin@flowupsolutions.com</span>
+          </div>
+
+          <div>
+            <span>+51 991316320 </span>
+          </div>
+
+          <div className="flex gap-[20px] md:gap-[14px] mt-[12px]">
+            <a
+              href="https://www.facebook.com/profile.php?id=61554718285482&locale=es_LA"
+              target="_blank"
+            >
+              <img src={navIcon1} alt="" />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/flow-up-solutions/?viewAsMember=true"
+              target="_blank"
+            >
+              <img src={navIcon2} alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div> @2023 Flous - Todos los derechos reservados</div>
+    </div>
+  );
+}
 
 export default Footer;
--- src/components/servicios/b-i/bi.js
+++ src/components/servicios/b-i/bi.js
@@ -1,130 +1,129 @@
-
 import React from "react";
-import bi from '../../../assets/busness-In.png'
-import './bi.css';
-import metodologia from '../../../assets/metodologia1.png';
-import gif from '../../../assets/gifts/etl.gif';
-import dasborad from '../../../assets/dasboard.png';
-import iconWhat from '../../../assets/icon-what.png'
-import img1 from '../../../assets/4230532-approve-check-cog-gear-pass-secure-tick_115010.png'
-import img2 from '../../../assets/analytics_data_visualization_market_trends_statistical_analysis_performance_metrics_icon_260966.png'
-import img3 from '../../../assets/automization_optimization_automation_process_task_delegation_workflow_automation_icon_261161.png'
-import img4 from '../../../assets/innovation_creativity_breakthrough_inventiveness_icon_261215.png'
+import bi from "../../../assets/busness-In.png";
+import "./bi.css";
+import metodologia from "../../../assets/metodologia1.png";
+import gif from "../../../assets/gifts/etl.gif";
+import dasborad from "../../../assets/dasboard.png";
+import iconWhat from "../../../assets/icon-what.png";
+import img1 from "../../../assets/4230532-approve-check-cog-gear-pass-secure-tick_115010.png";
+import img2 from "../../../assets/analytics_data_visualization_market_trends_statistical_analysis_performance_metrics_icon_260966.png";
+import img3 from "../../../assets/automization_optimization_automation_process_task_delegation_workflow_automation_icon_261161.png";
+import img4 from "../../../assets/innovation_creativity_breakthrough_inventiveness_icon_261215.png";
 
 function BusinessIntelligence() {
-    return (
-        <div>
-            <div className="banner">
-                <div className="texto">
-                    <p>
-                        Business Intelligence  </p>
-                    <br />
-
-                    <div>
-                        Somos expertos en convertir los datos de su
-                        organización en información y logramos obtener
-                        conocimiento mediante el análisis en tiempo real,
-                        respaldando así el proceso de toma de decisiones.
-                    </div>
-                    <br /> <br />  <br /> <a className='buttom' href="https://api.whatsapp.com/send?phone=51991316320&text=Flous" target="_blank">Comienza tu proyecto!!</a>
-                </div>
-
-                <div className="imagen-fondo fondo-img">
-                    <img src={bi} />
-                </div>
-
-                <a className='iconWhat' href="https://api.whatsapp.com/send?phone=51991316320&text=Flous" target="_blank">
-                    <img src={iconWhat} alt="Descripción de la imagen" width={90} />
-                </a>
-            </div>
-
-
-            <div className='sect2 sect-2-2-2 px-[20px]  pt-[30px]'>
-                <p className='title'>
-                    Beneficios de implementar la ingeniería de
-
-                    datos en tu negocio
-                </p>
-                <br />
-                <br />
-                <div className="servicios-bi1 grid md:grid-cols-4 grid-cols-2 gap-4 max-w-6xl mx-auto">
-                    <div className=' altura px-[15px] py-[20px] duration-150 hover:scale-[1.055] pointer'>
-                        <br />
-                        <img className="mx-auto" src={img1} />
-                        <br />
-                        <span>Toma de decisiones basados en datos</span>
-                    </div>
-                    <div className=' altura px-[15px] py-[20px] duration-150 hover:scale-[1.055] pointer'>
-                        <br />
-                        <img className="mx-auto" src={img2} />
-                        <br />
-                        <span>Eficiencia operativa</span>
-                    </div>
-                    <div className=' altura px-[15px] py-[20px] duration-150 hover:scale-[1.055] pointer'>
-                    <br/>
-                        <img className="mx-auto" src={img3} />
-                        <br />
-                        <span>Personalización y experiencia del cliente
-                        </span>
-                    </div>
-
-                    <div className=' altura px-[15px] py-[20px] duration-150 hover:scale-[1.055] pointer'>
-                    <br/>
-                        <img className="mx-auto" src={img4} />
-                        <br />
-                        <span>Innovación y competitividad
-                        </span>
-                    </div>
-
-
-                </div>
-            </div>
-
-            <div className='nuestraMetodologia mt-[50px] max-w-6xl mx-auto px-[20px] md:px-[0px]'>
-               
-                <p className='title'>
-                    Flujograma del proceso de datos
-                </p>
-                
-                <img className="w-full mt-[20px]" src={gif} />
-            </div>
-          
-            <div className='nuestraMetodologia mt-[50px] max-w-6xl mx-auto px-[20px] md:px-[0px]'>
-                
-                <img src={metodologia} />
-            </div>
-
-
-            <div className=' sect-bi  mt-[50px] max-w-6xl mx-auto px-[20px] md:px-[0px]'>
-                
-                <p className='title text-center'>
-                    Interactúa con este Dashboard
-                </p>
-
-                <p className="text-center">
-                    Te invitamos a que interactues con nuestra demo de un caso de éxito
-                </p>
-                <img className="mx-auto mt-[30px] w-full md:w-auto md:h-[400px]" src={dasborad} />
-
-            </div>
-
-            <div id='' className="sect3 mt-[50px]">
-                <div className=" texto text-footer max-w-6xl mx-auto ">
-                    
-
-                    <p>
-                        ¿Visualizas oportunidades brillantes en tus datos?</p>
-                    <br />
-
-                    <div>
-                        Estamos comprometidos a ofrecerte la solución más efectiva para impulsar la inteligencia empresarial de tu compañia. Descubre el potencial del Business Intelligence y transforma tus datos en decisiones estratégicas. ¡Optimiza tu visión con nosotros!.
-                    </div>
-                    <br /> <br />  <br /> <a className='buttom' href="https://outlook.office365.com/owa/calendar/FlowUpSolutions1@flowupsolutions.com/bookings/" target="_blank">¡Comienza tu proyecto!</a>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="banner">
+        <div className="texto">
+          <p>Business Intelligence </p>
+          <br />
+          <div>
+            Somos expertos en convertir los datos de su organización en
+            información y logramos obtener conocimiento mediante el análisis en
+            tiempo real, respaldando así el proceso de toma de decisiones.
+          </div>
+          <br /> <br /> <br />{" "}
+          <a
+            className="buttom"
+            href="https://api.whatsapp.com/send?phone=51991316320&text=Flous"
+            target="_blank"
+          >
+            Comienza tu proyecto!!
+          </a>
         </div>
-    )
 
+        <div className="imagen-fondo fondo-img">
+          <img src={bi} />
+        </div>
+
+        <a
+          className=" fixed bottom-[50px] md:bottom-[20px] right-[20px] transition-150 hover:scale-[1.15]"
+          href="https://api.whatsapp.com/send?phone=51991316320&text=Flous"
+          target="_blank"
+        >
+          <img src={iconWhat} alt="Descripción de la imagen" width={90} />
+        </a>
+      </div>
+
+      <div className="sect2 sect-2-2-2 px-[20px]  pt-[30px]">
+        <p className="title">
+          Beneficios de implementar la ingeniería de datos en tu negocio
+        </p>
+        <br />
+        <br />
+        <div className="servicios-bi1 grid md:grid-cols-4 grid-cols-2 gap-4 max-w-6xl mx-auto">
+          <div className=" altura px-[15px] py-[20px] duration-150 hover:scale-[1.055] pointer">
+            <br />
+            <img className="mx-auto" src={img1} />
+            <br />
+            <span>Toma de decisiones basados en datos</span>
+          </div>
+          <div className=" altura px-[15px] py-[20px] duration-150 hover:scale-[1.055] pointer">
+            <br />
+            <img className="mx-auto" src={img2} />
+            <br />
+            <span>Eficiencia operativa</span>
+          </div>
+          <div className=" altura px-[15px] py-[20px] duration-150 hover:scale-[1.055] pointer">
+            <br />
+            <img className="mx-auto" src={img3} />
+            <br />
+            <span>Personalización y experiencia del cliente</span>
+          </div>
+
+          <div className=" altura px-[15px] py-[20px] duration-150 hover:scale-[1.055] pointer">
+            <br />
+            <img className="mx-auto" src={img4} />
+            <br />
+            <span>Innovación y competitividad</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="nuestraMetodologia mt-[50px] max-w-6xl mx-auto px-[20px] md:px-[0px]">
+        <p className="title">Flujograma del proceso de datos</p>
+
+        <img className="w-full mt-[20px]" src={gif} />
+      </div>
+
+      <div className="nuestraMetodologia mt-[50px] max-w-6xl mx-auto px-[20px] md:px-[0px]">
+        <img src={metodologia} />
+      </div>
+
+      <div className=" sect-bi  mt-[50px] max-w-6xl mx-auto px-[20px] md:px-[0px]">
+        <p className="title text-center">Interactúa con este Dashboard</p>
+
+        <p className="text-center">
+          Te invitamos a que interactues con nuestra demo de un caso de éxito
+        </p>
+        <img
+          className="mx-auto mt-[30px] w-full md:w-auto md:h-[400px]"
+          src={dasborad}
+        />
+      </div>
+
+      <div id="" className="sect3 mt-[50px] px-[20px] md:px-0">
+        <div className=" texto text-footer max-w-6xl mx-auto pt-[40px] md:pt-[120px] flex flex-col">
+          <p className="">¿Visualizas oportunidades brillantes en tus datos?</p>
+          <br />
+          <div className="text-center">
+            Estamos comprometidos a ofrecerte la solución más efectiva para
+            impulsar la inteligencia empresarial de tu compañia. Descubre el
+            potencial del Business Intelligence y transforma tus datos en
+            decisiones estratégicas. ¡Optimiza tu visión con nosotros!.
+          </div>
+        
+          <a
+            className="buttom w-max mt-[64px] mx-auto"
+            href="https://outlook.office365.com/owa/calendar/FlowUpSolutions1@flowupsolutions.com/bookings/"
+            target="_blank"
+          >
+            ¡Comienza tu proyecto!
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BusinessIntelligence;
